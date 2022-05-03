2938e30452ae28a233c07291a018abd9a18d061e
YazanAbbas1 <98409415+YazanAbbas1@users.noreply.github.com>
2022-05-04 01:54:51 +0200

    up to date

--- Flex.php
+++ Flex.php
@@ -2,7 +2,7 @@
 require "dbConfig.php";
 $db = new dbConfig();
 $newconnection=mysqli_connect($db->host,$db->username,$db->password,$db->databasename);
-
+mysqli_set_charset($newconnection,"utf8");
 if(isset($_POST['username']) && isset($_POST['start_date'])){
 
     $uname = $_POST['username'];
@@ -11,16 +11,17 @@ if(isset($_POST['username']) && isset($_POST['start_date'])){
     $end_date = $_POST['end_date'];
     $start_time = $_POST['start_time'];
     $end_time = $_POST['end_time'];
+    $Emsg = $_POST['Emsg'];
     $msg = $_POST['msg']; 
 
     $value ='0'; 
 
-    $sql = "select * from flextabell where username = '" . $uname . "'";
+    $sql = "select * from flex where username = '" . $uname . "'";
     $result = mysqli_query($newconnection,$sql);
     $row = mysqli_fetch_assoc($result);
 
     if(mysqli_num_rows($result) == 0){
-        $sql = "INSERT INTO flextabell ( username, quantity, start_date, end_date, start_time, end_time, msg, value) VALUES ( '$uname','$quantity', '$start_date',  '$end_date', '$start_time', '$end_time', '$value')";
+        $sql = "INSERT INTO flex ( username, quantity, start_date, end_date, start_time, end_time, msg,Emsg, value) VALUES ( '$uname','$quantity', '$start_date',  '$end_date', '$start_time', '$end_time', '$msg','$Emsg','$value')";
 
         $result = mysqli_query($newconnection,$sql);
         if($result)
@@ -32,3 +33,4 @@ if(isset($_POST['username']) && isset($_POST['start_date'])){
     }else echo "You already have an ongoing activity"; 
 
 }else echo "You need to choose an available start date and Start Time";
+?>
